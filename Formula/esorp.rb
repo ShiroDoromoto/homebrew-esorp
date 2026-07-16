7ae3e2031223a2f131f5e0c4041b4893797ac26a
ShiroDoro <ShiroDoromoto@users.noreply.github.com>
2026-07-16 19:57:16 +0900

    wharfy: update esorp 0.6.0

--- Formula/esorp.rb
+++ Formula/esorp.rb
@@ -1,27 +1,27 @@
 class Esorp < Formula
   homepage "https://github.com/ShiroDoromoto/esorp"
-  version "0.5.0"
+  version "0.6.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.5.0/esorp_0.5.0_darwin_arm64.tar.gz"
-      sha256 "9ad37fb254ff970b43025b4059bde61e83518c10b25e2a2ab1a2df745f9c19ca"
+      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.6.0/esorp_0.6.0_darwin_arm64.tar.gz"
+      sha256 "74423f512b8c55d71b4c5974d1d473268351bb5d06a87e158f11de5e0b6d1d5e"
     end
     on_intel do
-      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.5.0/esorp_0.5.0_darwin_amd64.tar.gz"
-      sha256 "cf2424c6021f7fb5818452e48c163db8e476f76492f8f860c51e22d26d978204"
+      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.6.0/esorp_0.6.0_darwin_amd64.tar.gz"
+      sha256 "f0e6cc27dc4450c118e41c1b46d8116f56ef80d7038ef1ee0a2404540d26dbbf"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.5.0/esorp_0.5.0_linux_arm64.tar.gz"
-      sha256 "e08a00cd03cdc54f924f4166f0199276ee631bf9199fa8b7747794f1b8c5c143"
+      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.6.0/esorp_0.6.0_linux_arm64.tar.gz"
+      sha256 "86d84b0c5847c54ca17f568739239cb480b95b47cae4bec69a5a33c98206334b"
     end
     on_intel do
-      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.5.0/esorp_0.5.0_linux_amd64.tar.gz"
-      sha256 "fd30627a38c0d07be0dbcb66dc8b4f9cfc40e6d2c8fb761f75d7afac02fdd78a"
+      url "https://github.com/ShiroDoromoto/esorp/releases/download/v0.6.0/esorp_0.6.0_linux_amd64.tar.gz"
+      sha256 "bd0a8136aa859892e21b51270ba6202e40529e100c0e8aeb5558726822e257c2"
     end
   end
 
@@ -30,6 +30,6 @@ class Esorp < Formula
   end
 
   test do
-    assert_match "0.5.0", shell_output("#{bin}/esorp version")
+    assert_match "0.6.0", shell_output("#{bin}/esorp version")
   end
 end
